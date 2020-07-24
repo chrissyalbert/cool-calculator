ab40e42d3141e277d96227dac493127262f0b9fc
chrissyalbert <chrissy_albert@yahoo.com>
2020-07-24 16:01:15 -0700

    Updates

---
+++ precache-manifest.43878e13fcb27d0c681c19bcf9dddb12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c489035acc25380d130c7020bcc3536",
+    "url": "/cool-calculator/index.html"
+  },
+  {
+    "revision": "9dea884947c48aaefa14",
+    "url": "/cool-calculator/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8826da950defcf195ed6",
+    "url": "/cool-calculator/static/css/main.78027c9d.chunk.css"
+  },
+  {
+    "revision": "9dea884947c48aaefa14",
+    "url": "/cool-calculator/static/js/2.b9fdebf2.chunk.js"
+  },
+  {
+    "revision": "ac708def751a8292e55daf5717e57737",
+    "url": "/cool-calculator/static/js/2.b9fdebf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8826da950defcf195ed6",
+    "url": "/cool-calculator/static/js/main.3344b1a2.chunk.js"
+  },
+  {
+    "revision": "945d4a469b72647f254f",
+    "url": "/cool-calculator/static/js/runtime-main.4753216c.js"
+  }
+]);
